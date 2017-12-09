e575539e9eb2d414b2e732cd6ee147bc9d80b719
abolfazl <bateternal@gmail.com>
2017-12-09 14:51:06 +0330

    main

--- main.go
+++ main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"./DataStructure"
-	//"fmt"
-
-	"fmt"
 )
 
 func main() {
-	fmt.Println(DataStructure.Create())
+	ll := DataStructure.LinkListAgency{}
+	a := DataStructure.Agency{AgencyName:"Car"}
+	b := DataStructure.Agency{AgencyName:"def"}
+	ll.AddAgency(&a)
+	ll.AddAgency(&b)
+	l := DataStructure.LinkListService{}
+	c := DataStructure.Service{ServiceName:"c"}
+	d := DataStructure.Service{ServiceName:"d"}
+	e := DataStructure.Service{ServiceName:"e"}
+	h := DataStructure.Service{ServiceName:"h"}
+	i := DataStructure.Service{ServiceName:"i"}
+	l.AddService(&c)
+	//d.AddSubService(&e)
+	l.AddService(&d)
+	l.AddService(&h)
+	d.AddSubService(&e)
+	d.AddSubService(&i)
+	l.ListServices()
 }
 
