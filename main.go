6eefae0252a6840984cc005e52fb84eeb4068614
abolfazl <bateternal@gmail.com>
2017-12-18 15:06:13 +0330

    main

--- main.go
+++ main.go
@@ -7,47 +7,78 @@ import (
 	"os"
 	"fmt"
 	"strings"
-	_ "go/types"
-	"reflect"
 )
 
 
 var linkListService DataStructure.LinkListService
 var linkListAgency DataStructure.LinkListAgency
+var maxHeap DataStructure.MaxHeap
 func main() {
-	command := start()
-	select {
-	case len(command)==3 && command[0]=="add" && command[1] == "service":
-		AddService(command[2])
+	linkListService = DataStructure.LinkListService{}
+	linkListAgency = DataStructure.LinkListAgency{}
+	for {
+		command := start()
+		if len(command) == 3 && command[0] == "add" && command[1] == "service" {
+			AddService(command[2])
+		} else if len(command) == 5 && command[0] == "add" && command[1] == "subservice" && command[3] == "to" {
+			AddSubService(command[4], command[2])
+		}else if len(command) == 5 && command[0] == "add" && command[1] == "offer" && command[3] == "to" {
+			AddOffer(command[2],command[4])
+		}else{
+			error()
+		}
 	}
 }
 
 func start() []string {
-	linkListService = DataStructure.LinkListService{}
-	linkListAgency = DataStructure.LinkListAgency{}
+	maxHeap = *DataStructure.New()
 	reader := bufio.NewReader(os.Stdin)
 	//fmt.Println(reflect.TypeOf(reader))
 	fmt.Print("Enter Command: ")
 	text, _ := reader.ReadString('\n')
 	text = string(text)
-	com := strings.Split(text, "")
+	com := strings.Split(text, " ")
 	return com
 }
 
 func AddService (serviceName string){
 	s := DataStructure.Service{ServiceName:serviceName}
-	linkListService.AddService(&s)
-}
-
-func AddSubservice (serviceName string,subServiceName string){
 	service := linkListService.Search(serviceName)
-	Service := DataStructure.Service{ServiceName:subServiceName}
-	service.AddSubService(&Service)
+	if service == nil {
+		linkListService.AddService(&s)
+	}else{
+		fmt.Println("This ServiceName already used!")
+	}
 }
 
+func AddSubService (serviceName string,subServiceName string){
+	service := linkListService.Search(serviceName)
+	if service != nil {
+		Service := DataStructure.Service{ServiceName: subServiceName}
+		service.AddSubService(&Service)
+	}else{
+		fmt.Println(serviceName + " not found!")
+	}
+
+}
+
+func AddOffer(serviceName string,agencyName string){
+	service := linkListService.Search(serviceName)
+	agency := linkListAgency.Search(agencyName)
+	if service == nil && agency == nil{
+		fmt.Println(agencyName + " and " + serviceName + " not found!")
+	}else if service == nil{
+		fmt.Println(serviceName + " not found!")
+	}else if agency == nil{
+		fmt.Println(agencyName + "not found!")
+	}else{
+		agency.AddOffer(service)
+	}
+}
 func error(){
 	fmt.Println("invalid command!")
 }
+
 
 
 //func main() {
